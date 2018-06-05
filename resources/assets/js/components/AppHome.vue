94572f1a8771508d4e8a8d4f4d0c949a8c3bc36c
jredli <redlijovan@gmail.com>
2018-06-05 22:00:12 +0200

    Login component and Login

--- resources/assets/js/components/AppHome.vue
+++ resources/assets/js/components/AppHome.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
         <toolbar></toolbar>
-        <app-footer></app-footer>
+        <router-view></router-view>
+        <app-footer></app-footer>        
     </div>
 </template>
 
 <script>
 import toolbar from './Toolbar'
 import AppFooter from './AppFooter'
+import Login from './login/Login'
 
 export default {
-  components:{toolbar, AppFooter}
+  components:{toolbar, AppFooter, Login}
 }
 </script>
 
